e6d5d6127fc0429f91255d69c553fdd79b93a5a9
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-12-08 15:49:16 +0300

    Main page prefab
    TODO: DB

--- activity-directorship-emu/components/shared/MobileNav.tsx
+++ activity-directorship-emu/components/shared/MobileNav.tsx
@@ -17,7 +17,7 @@ const MobileNav = () => {
                     <Image src="/assets/icons/menu.svg" alt="Logo" width={24} height={24} className="cursor-pointer"/>
                 </SheetTrigger>
                 <SheetContent className="flex flex-col gap-6 bg-white md:hidden">
-                    <Image src="/assets/images/logo.svg" alt="Logo"  width={128} height={38}/>
+                    <Image src="/assets/activity-directorship-emu-high-resolution-logo.png" alt="Logo"  width={128} height={38}/>
                     <Separator className="border border-gray-50"/>
                     <NavItems />
 
