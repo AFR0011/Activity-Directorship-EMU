81a08caf33385ef480808b9ed6981dfc81230a9a
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-12-12 14:55:28 +0300

    Event details page(WIP)

--- activity-directorship-emu/lib/actions/event.actions.ts
+++ activity-directorship-emu/lib/actions/event.actions.ts
@@ -1,25 +1,50 @@
 "use server"
 
-import { CreateEventParams } from "@/types"
-import { handleError } from "../utils"
-import { connectToDatabase } from "../database"
-import User from "../database/models/user.model"
+import { connectToDatabase } from '@/lib/database'
+import Event from '@/lib/database/models/event.model'
+import User from '@/lib/database/models/user.model'
+import Category from '@/lib/database/models/category.model' //Does DB even have this ???
+import { handleError } from '@/lib/utils'
+import { CreateEventParams } from '@/types'
+
+const populateEvent = async (query: any) => {
+    return query
+        .populate({ path: 'organizer', model: 'User', select: '_id firstName lastName' })
+        .populate({ path: 'category', model: 'Category', select: '_id name' })
+
+}
 
 
-export const createEvent = async ({ event, userId, path}: 
+export const createEvent = async ({ event, userId, path }:
     CreateEventParams) => {
-        try {
-            await connectToDatabase();
+    try {
+        await connectToDatabase();
 
-            const organizer = await User.findById(userId);
-            if(!organizer) {
-                throw new Error("Organizer not found!");
-            }
-
-            const newEvent = await Event.create({...event, category: event.categoryId, organizer: userId}); //DB may not have this; CHECK DB
-
-            return JSON.parse(JSON.stringify(newEvent));
-        } catch(e) {
-            handleError(e);
+        const organizer = await User.findById(userId);
+        if (!organizer) {
+            throw new Error("Organizer not found!");
         }
+
+        const newEvent = await Event.create({ ...event, category: event.categoryId, organizer: userId }); //DB may not have this; CHECK DB
+
+        return JSON.parse(JSON.stringify(newEvent));
+    } catch (e) {
+        handleError(e);
     }
+}
+
+export const getEventById = async (eventId: string) => {
+    try {
+        await connectToDatabase();
+
+        const event = await populateEvent(Event.findById(eventId));
+
+        if (!event) {
+            throw new Error("Event not found!");
+        }
+
+        return JSON.parse(JSON.stringify(event))
+    } catch (e) {
+        handleError(e);
+    }
+}
