7ac4b47aa3c1fa0c883aace279105c94204031dc
Ali Farrokhnejad <farrokh445@gmail.com>
2024-12-14 12:06:38 +0200

    Deployment test final

--- activity-directorship-emu/app/(root)/events/[id]/update/page.tsx
+++ activity-directorship-emu/app/(root)/events/[id]/update/page.tsx
@@ -1,36 +1,41 @@
-import EventForm from "@/components/shared/EventForm"
-import { getEventById } from "@/lib/actions/event.actions";
-import { auth } from "@clerk/nextjs/server"
+// import EventForm from "@/components/shared/EventForm"
+// import { getEventById } from "@/lib/actions/event.actions";
+// import { auth } from "@clerk/nextjs/server"
 
-type UpdateEventProps = {
-  params: {
-    id: string
-  }
+// type UpdateEventProps = {
+//   params: {
+//     id: string
+//   }
+// }
+
+const UpdateEvent = () => {
+  return(
+    <div></div>
+  )
 }
 
+// const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
 
-const UpdateEvent = async ({ params: { id } }: UpdateEventProps) => {
 
+//   const userId = sessionClaims?.userId as string;
+//   const event = await getEventById(id);
 
-  const userId = sessionClaims?.userId as string;
-  const event = await getEventById(id);
+//   const { sessionClaims } = await auth();
 
-  const { sessionClaims } = await auth();
+//   return (
 
-  return (
+//     <>
+//       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
+//         <h3 className="wrapper h3-bold text-center sm:text-left">Update Event</h3>
+//       </section>
 
-    <>
-      <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-        <h3 className="wrapper h3-bold text-center sm:text-left">Update Event</h3>
-      </section>
+//       <div className="wrapper my-8">
+//         <EventForm userId={userId} type="Update" event={event} eventId={event._id}/>
+//       </div>
 
-      <div className="wrapper my-8">
-        <EventForm userId={userId} type="Update" event={event} eventId={event._id}/>
-      </div>
-
-    </>
-  ) 
-}
+//     </>
+//   ) 
+// }
 
 
 export default UpdateEvent
