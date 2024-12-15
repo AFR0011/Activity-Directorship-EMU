2f5f5a073341e01f5b34080bee30dfc8a0f5d377
Morteza Farrokhnejad <152209810+IAmFarrokhnejad@users.noreply.github.com>
2024-12-15 22:10:20 +0300

    Pagination done
    Project is finalized at this point, however, we need to fix the final bugs and do some design.

--- activity-directorship-emu/components/shared/Collections.tsx
+++ activity-directorship-emu/components/shared/Collections.tsx
@@ -1,5 +1,6 @@
 import { IEvent } from "@/lib/database/models/event.model"
 import Card from "./Card"
+import Pagination from "./Pagination"
 
 type CollectionProps = {
   data: IEvent[],
@@ -12,7 +13,7 @@ type CollectionProps = {
   collectionType?: 'Events_Organized' | 'My_Tickets' | 'All_Events'
 }
 
-const Collections = ({ data, emptyTitle, emptyStateSubtext, page, totalPages = 0, collectionType, urlParamName }: CollectionProps) => {
+const Collections = ({ data, emptyTitle, emptyStateSubtext, page, totalPages = 0, collectionType, urlParamName, }: CollectionProps) => {
   return (
     <>
       {data.length > 0 ? (
@@ -29,6 +30,10 @@ const Collections = ({ data, emptyTitle, emptyStateSubtext, page, totalPages = 0
             )
             })}
           </ul>
+
+          {totalPages > 1 && (
+            <Pagination urlParamName={urlParamName} page={page} totalPages={totalPages}/>
+          )}
 
         </div>
       ) : (
